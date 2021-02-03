244db1ff0a85092b8412d56966789b4de77927d9
Pete-King <66584370+BigPete-King@users.noreply.github.com>
2021-02-03 10:28:27 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module CloudFunction-Download
+
+go 1.14
+
+require (
+	github.com/tencentyun/cos-go-sdk-v5 v0.7.17
+	github.com/tencentyun/scf-go-lib v0.0.0-20200624065115-ba679e2ec9c9
+)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/tencentyun/cos-go-sdk-v5"
+	"github.com/tencentyun/scf-go-lib/cloudfunction"
+	"github.com/tencentyun/scf-go-lib/events"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type DownloadParam struct {
+	URL string `json：url`
+}
+
+func download(ctx context.Context, request events.APIGatewayRequest) (events.APIGatewayResponse, error) {
+
+	domain := os.Getenv("COS_DOMAIN")
+	secretID := os.Getenv("COS_SECRETID")
+	secretKey := os.Getenv("COS_SECRETKEY")
+
+	var downloadURL string
+	if request.Body != "" {
+		var body DownloadParam
+		if err := json.Unmarshal([]byte(request.Body), &body); err != nil {
+			return events.APIGatewayResponse{}, err
+		}
+		downloadURL = body.URL
+	}
+
+	if downloadURL == "" {
+		return events.APIGatewayResponse{}, errors.New("缺少URL参数")
+	}
+
+	resp, err := http.Get(downloadURL)
+	if err != nil {
+		return events.APIGatewayResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return events.APIGatewayResponse{}, errors.New("下载失败")
+	}
+
+	var fileName string
+	if contentDisposition := resp.Header.Get("Content-Disposition"); contentDisposition != "" {
+		if r, err := regexp.Compile("filename=(.*)"); err == nil {
+			if subMatch := r.FindStringSubmatch(contentDisposition); subMatch != nil {
+				fileName = subMatch[1]
+			}
+		}
+	}
+
+	if fileName == "" {
+		fileName = downloadURL[strings.LastIndex(downloadURL, "/")+1 : strings.Index(downloadURL, "?")]
+	}
+	fmt.Println("fileName:" + fileName)
+	bucketURL, err := url.Parse(domain)
+	if err != nil {
+		return events.APIGatewayResponse{}, err
+	}
+	baseURL := &cos.BaseURL{BucketURL: bucketURL}
+	client := cos.NewClient(baseURL, &http.Client{
+		//设置超时时间
+		Timeout: 900 * time.Second,
+		Transport: &cos.AuthorizationTransport{
+			//如实填写账号和密钥，也可以设置为环境变量
+			SecretID:  secretID,
+			SecretKey: secretKey,
+		},
+	})
+	_, err = client.Object.Put(context.Background(), fileName, resp.Body, nil)
+	if err != nil {
+		return events.APIGatewayResponse{}, err
+	}
+	result, _ := json.Marshal(DownloadParam{
+		URL: domain + "/" + fileName,
+	})
+
+	return events.APIGatewayResponse{
+		IsBase64Encoded: false,
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "application/json"},
+		Body:            string(result),
+	}, nil
+}
+
+func main() {
+
+	// Make the handler available for Remote Procedure Call by Cloud Function
+	cloudfunction.Start(download)
+
+}
